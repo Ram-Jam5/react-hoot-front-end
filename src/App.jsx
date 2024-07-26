d074617a8ed2f20c693d135af45a1befecb7b0a0
Ram-Jam5 <edgeillusive@gmail.com>
2024-07-26 11:24:43 -0500

    complete delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,53 @@
-import { useState, createContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useState, createContext, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
+import HootList from './components/HootList/HootList';
+import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
 import * as authService from '../src/services/authService'; // import the authservice
+import * as hootService from './services/hootService';
 
 export const AuthedUserContext = createContext(null);
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+  const [hoots, setHoots] = useState([])
+  const navigate = useNavigate();
 
+  
+  
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots();
+  }, [user]);
+  
   const handleSignout = () => {
     authService.signout();
     setUser(null);
   };
+  
+  const handleAddHoot = async (hootFormData) => {
+    const newHoot = await hootService.create(hootFormData);
+    setHoots([newHoot, ...hoots]);
+    navigate('/hoots');
+  };
+
+  
+const handleDeleteHoot = async (hootId) => {
+  // Call upon the service function:
+  const deletedHoot = await hootService.deleteHoot(hootId);
+  // Filter state using deletedHoot._id:
+  setHoots(hoots.filter((hoot) => hoot._id !== deletedHoot._id));
+  // Redirect the user:
+  navigate('/hoots');
+};
 
   return (
     <>
@@ -23,9 +55,15 @@ const App = () => {
         <NavBar user={user} handleSignout={handleSignout} />
         <Routes>
           {user ? (
+            <>
             <Route path="/" element={<Dashboard user={user} />} />
+            <Route path="/hoots" element={<HootList hoots={hoots} />} />
+            <Route path="/hoots/:hootId" element={<HootDetails handleDeleteHoot={handleDeleteHoot} />} />
+            <Route path="/hoots/new" element={<HootForm handleAddHoot={handleAddHoot} />} />
+            
+            </>
           ) : (
-            <Route path="/" element={<Landing />} />
+            <Route path="/" element={<Landing />} />  
           )}
           <Route path="/signup" element={<SignupForm setUser={setUser} />} />
           <Route path="/signin" element={<SigninForm setUser={setUser} />} />
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,9 +9,14 @@ const NavBar = ({ handleSignout }) => {
       {user ? (
         <nav>
           <ul>
-            <li>Welcome, {user.username}</li>
             <li>
-              <Link to="/">Dashboard</Link>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/hoots">HOOTS</Link>
+            </li>
+            <li>
+              <Link to="/hoots/new">NEW HOOT</Link>
             </li>
             <li>
               <Link to="" onClick={handleSignout}>
